fd7f2dae2317a6670090b3ec596f118e633f2ccd
Mehdi Chaid <JeyzerMC@users.noreply.github.com>
2026-05-07 00:06:17 -0400

    fix(ui): revert flake_stack.js to working version

--- web/flake_stack.js
+++ web/flake_stack.js
@@ -11,30 +11,18 @@ function removeHiddenInputs(node, names) {
             node.inputs.splice(i, 1);
         }
     }
-}
-
-function disableWidgetConversion(node, names) {
-    const origGetWidgetOnPos = node.getWidgetOnPos;
-    node.getWidgetOnPos = function (x, y) {
-        const widget = origGetWidgetOnPos?.apply(this, arguments);
-        if (widget && names.includes(widget.name)) {
-            return null;
-        }
-        return widget;
-    };
+    node.setSize(node.computeSize());
 }
 
 app.registerExtension({
     name: "comfyui-flakes.FlakeStack",
     async beforeRegisterNodeDef(nodeType, nodeData) {
         if (nodeData.name === "FlakeStack") {
-            nodeType.prototype.size = [340, 200];
             const origOnNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 const r = origOnNodeCreated?.apply(this, arguments);
                 setupFlakeWidget(this);
                 removeHiddenInputs(this, ["model_family", "flakes_json"]);
-                disableWidgetConversion(this, ["model_family", "flakes_json"]);
                 return r;
             };
 
@@ -42,19 +30,16 @@ app.registerExtension({
             nodeType.prototype.onConfigure = function () {
                 const r = origOnConfigure?.apply(this, arguments);
                 removeHiddenInputs(this, ["model_family", "flakes_json"]);
-                disableWidgetConversion(this, ["model_family", "flakes_json"]);
                 this._flakes_render?.();
                 return r;
             };
         }
         if (nodeData.name === "FlakeModelPreset") {
-            nodeType.prototype.size = [300, 200];
             const origOnNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 const r = origOnNodeCreated?.apply(this, arguments);
                 setupFlakeModelPresetWidget(this);
                 removeHiddenInputs(this, ["model_family", "preset"]);
-                disableWidgetConversion(this, ["model_family", "preset"]);
                 return r;
             };
 
@@ -62,19 +47,16 @@ app.registerExtension({
             nodeType.prototype.onConfigure = function () {
                 const r = origOnConfigure?.apply(this, arguments);
                 removeHiddenInputs(this, ["model_family", "preset"]);
-                disableWidgetConversion(this, ["model_family", "preset"]);
                 this._preset_render?.();
                 return r;
             };
         }
         if (nodeData.name === "FlakeCombo") {
-            nodeType.prototype.size = [340, 200];
             const origOnNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 const r = origOnNodeCreated?.apply(this, arguments);
                 setupFlakeComboWidget(this);
                 removeHiddenInputs(this, ["model_family", "flakes_json"]);
-                disableWidgetConversion(this, ["model_family", "flakes_json"]);
                 return r;
             };
 
@@ -82,19 +64,16 @@ app.registerExtension({
             nodeType.prototype.onConfigure = function () {
                 const r = origOnConfigure?.apply(this, arguments);
                 removeHiddenInputs(this, ["model_family", "flakes_json"]);
-                disableWidgetConversion(this, ["model_family", "flakes_json"]);
                 this._combo_render?.();
                 return r;
             };
         }
         if (nodeData.name === "FlakeModelCombo") {
-            nodeType.prototype.size = [300, 200];
             const origOnNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 const r = origOnNodeCreated?.apply(this, arguments);
                 setupFlakeModelComboWidget(this);
                 removeHiddenInputs(this, ["model_family", "preset"]);
-                disableWidgetConversion(this, ["model_family", "preset"]);
                 return r;
             };
 
@@ -102,7 +81,6 @@ app.registerExtension({
             nodeType.prototype.onConfigure = function () {
                 const r = origOnConfigure?.apply(this, arguments);
                 removeHiddenInputs(this, ["model_family", "preset"]);
-                disableWidgetConversion(this, ["model_family", "preset"]);
                 this._model_combo_render?.();
                 return r;
             };
